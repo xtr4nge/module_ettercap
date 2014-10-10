c8a5b17924f835465ca580ac9f68b0c1599d0f10
xtr4nge <xtr4nge@gmail.com>
2014-10-11 00:43:05 +0100

    Initial commit

--- includes/install.sh
+++ includes/install.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 echo "installing ettercap..."
-apt-get -y install ettercap
+#apt-get -y install ettercap
+
+apt-get -y install unzip
+apt-get -y install cmake libncurses5-dev libssl-dev libpcap-dev flex bison libgtk2.0-dev
+
+wget https://github.com/Ettercap/ettercap/archive/v0.8.0.tar.gz
+tar zxvf v0.8.0.tar.gz
+cd ettercap-0.8.0
+mkdir build
+cd build
+cmake ../
+make
+make install
+ln -s /usr/local/bin/ettercap /usr/sbin/ettercap
 
 echo "..DONE.."
 exit
